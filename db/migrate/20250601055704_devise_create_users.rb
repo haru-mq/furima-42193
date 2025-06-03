85c1a2dd553e65e6777d16e5a7408bbc01e66092
harubo <haru.act01@gmail.com>
2025-06-03 19:52:47 +0900

    usersテーブルの修正

--- db/migrate/20250601055704_devise_create_users.rb
+++ db/migrate/20250601055704_devise_create_users.rb
@@ -9,11 +9,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.1]
       t.string :encrypted_password, null: false, default: ""
       t.string :first_name,         null: false, default: ""
       t.string :last_name,          null: false, default: ""
-      t.string :first_name,         null: false, default: ""
-      t.string :last_name,          null: false, default: ""
       t.string :first_name_kana,    null: false, default: ""
       t.string :last_name_kana,     null: false, default: ""
-      t.date   :birthday,           null: false, default: ""
+      t.date   :birthday,           null: false
 
       ## Recoverable
       t.string   :reset_password_token
