e35d8a535150d82c0818334d1133b91edf30ae73
harubo <haru.act01@gmail.com>
2025-06-03 21:00:32 +0900

    ログイン/ログアウトのビューの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-   validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'を入力してください' }
-   validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'を入力してください' }
-   validates :first_name_kana, format: { with: /\A[ァ-ヶ一]+\z/, message: 'を入力してください' }
-   validates :last_name_kana, format: { with: /\A[ァ-ヶ一]+\z/, message: 'を入力してください' }
+         validates :first_name, presence: true,format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'を入力してください' }
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'を入力してください' }
+         validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'を入力してください' }
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'を入力してください' }
 
 
 end
