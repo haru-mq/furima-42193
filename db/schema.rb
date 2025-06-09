c508356ca22c0f98b6524df00ad2df1a2b079a77
harubo <haru.act01@gmail.com>
2025-06-09 20:52:38 +0900

    データベースの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_01_055704) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_09_113153) do
+  create_table "items", charset: "utf8mb3", force: :cascade do |t|
+    t.string "item_name", null: false
+    t.text "item_description", null: false
+    t.integer "category_id", null: false
+    t.integer "condition_id", null: false
+    t.integer "fee_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "ship_date_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id_id"], name: "index_items_on_user_id_id"
+  end
+
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
