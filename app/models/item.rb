9bfe3351dc214d04f4fa3feb42b5f172bb190ec7
harubo <haru.act01@gmail.com>
2025-06-09 20:53:00 +0900

    Active Hashの導入

---
+++ app/models/item.rb
@@ -0,0 +1,21 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Association::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :fee
+  belongs_to :prefecture
+  belongs_to :ship_date
+
+  belong_to :user
+
+  validates :item_name,         presence: true
+  validates :item_description,  presence: true
+  validates :category_id,       numericality{ other_than: 1 }
+  validates :condition_id,      numericality{ other_than: 1 }
+  validates :fee_id,            numericality{ other_than: 1 }
+  validates :prefecture_id,     numericality{ other_than: 1 }
+  validates :ship_date_id,      numericality{ other_than: 1 }
+  validates :price,             numericality{ only_integer: true, greater_than: 300 }
+  validates :user_id,           presence: true
+
+end
