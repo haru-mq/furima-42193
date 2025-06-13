665f405078f65f87936e0407836f61af938db257
harubo <haru.act01@gmail.com>
2025-06-13 19:45:15 +0900

    販売金額と販売手数料計算の実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,17 @@
-window.addEventListener('turbo:load', () => {
-  console.log("Hello");
-});
+const price = () => {
+  const priceInput = document.getElementById('item-price');
+
+  priceInput.addEventListener('input', () => {
+    const inputValue = priceInput.value;
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
+
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = Math.floor(inputValue * 0.9);
+
+  })
+};
+
+window.addEventListener("turbo:load", price);
+window.addEventListener("turbo:render", price);
