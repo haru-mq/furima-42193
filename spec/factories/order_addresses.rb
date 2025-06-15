e03ca79a9b2f6dfeb57c8ae7a520b505bd004da7
harubo <haru.act01@gmail.com>
2025-06-15 21:41:16 +0900

    単体テストの再構築

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
-  factory :record_address do
+  factory :order_address do
 
     post_number     {"#{rand(100..999)}-#{rand(1000..9999)}"}
     prefecture_id   {rand(2..48)}
@@ -7,7 +7,6 @@ FactoryBot.define do
     house_number    {Faker::Address.building_number()}
     building        {"#{Faker::Name.last_name()}ビル"}
     phone_number    {Faker::Number.number(digits: 11)}
-    token           {"tok_abcdefghijk00000000000000000"}
 
     association :user
     association :item
