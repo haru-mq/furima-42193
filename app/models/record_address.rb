5758f5524d62fdc83882234554181b41be821295
harubo <haru.act01@gmail.com>
2025-06-15 13:12:22 +0900

    バリデーションの修正

--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -4,11 +4,11 @@ class RecordAddress
   attr_accessor :user_id, :item_id, :post_number, :prefecture_id, :city, :house_number, :building, :phone_number, :record_id,:token
   validates :user_id,         presence: true
   validates :item_id,         presence: true  
-  validates :post_number,     presence: true
+  validates :post_number,     presence: true, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, messages: 'is invalid. Input correctly.'}
   validates :prefecture_id,   numericality: { other_than: 1 }
   validates :city,            presence: true
   validates :house_number,    presence: true
-  validates :phone_number,    presence: true
+  validates :phone_number,    presence: true, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid. Input only numbers.'}
   validates :token,           presence: true
 
   def save
