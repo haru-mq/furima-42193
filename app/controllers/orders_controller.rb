28906009a0883b21b2f6aef316c78489c3250c7b
harubo <haru.act01@gmail.com>
2025-06-15 22:55:36 +0900

    コントローラーの整備

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,14 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+  before_action :redirect_if_owner, only: [:index, :create]
+
   def index
     gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(set_params)
     if @order_address.valid?
       pay_item
@@ -20,6 +22,10 @@ class OrdersController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def set_params
     params.require(:order_address).permit(:post_number, :prefecture_id, :city, :house_number, :building, :phone_number, :record_id, :token).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
@@ -31,6 +37,12 @@ class OrdersController < ApplicationController
       card:     set_params[:token],
       currency: "jpy"
     )
+  end
+
+  def redirect_if_owner
+    if current_user.id == @item.user_id
+      redirect_to '/'
+    end
   end
 
 end
