13f46abfbfea0f7b2aadcb8fd85f006aa49ce8e9
harubo <haru.act01@gmail.com>
2025-06-26 20:05:57 +0900

    修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,3 +95,5 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
+
+server '52.192.60.154/', user: 'ec2-user', roles: %w{app db web}
